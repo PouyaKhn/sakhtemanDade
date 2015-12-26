40e1da098d07359b493353a182281a0dffdf4a3f
pouyakhn <pouya.khani11@gmail.com>
2015-12-25 23:06:26 -0800

    Commit 1.1

---
+++ myProject/student.h
@@ -0,0 +1,33 @@
+//student class
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<iostream>
+class student{
+protected:
+	char name[15];
+	char family[15];
+	char fatherName[15];
+	char address[100];
+	long int homeNumber;
+	long int mobileNumber;
+	enum course{
+		computerEng, electricalEng, mechanicalEng, civilEng, industrialEng, chemicalEng, architecturalEng, miningEng,
+		materialsEng,persianLanguage,arabicLanguage,englishTranslating,theology,law,manage,math,statistics,physics,
+	    computerScience,chemistry,biotech};
+	course field;
+	enum marriageCondition { married,single,divorced };
+	marriageCondition mc;
+	enum college { engineering,sciences,humanities,nano};
+	college faculty;
+	enum section { BS,MSc,phD };
+	section grade;
+	enum situation { daily, nightly};
+	situation status;
+	int entrance;
+	char stundentNumber[7];
+	int failedTermNumber;
+	double lastTermRate;
+	int term;
+};
+
+#endif
