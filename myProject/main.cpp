ca2fb225458fc243899612648e8bf8bb7f727ef1
pouyakhn <pouya.khani11@gmail.com>
2015-12-25 22:49:50 -0800

    Commit 1.0

--- myProject/main.cpp
+++ myProject/main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
-
+#include "functions.h" //functions that are needed for menu
+#include "student.h" //student class
 using namespace std;
 
 int main(){
+	int menuSwitch;
+	menuSwitch=login();
+	//switch case meghdare menuSwitch baraye nemayeshe menu ye portal
 	return 0;
 }
